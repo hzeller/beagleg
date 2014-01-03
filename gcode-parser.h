df82b598806fc9f12561bfdf813948b178db7fb2
Henner Zeller <h.zeller@acm.org>
2014-01-03 06:59:18 +0000

    o add LICENSE

--- gcode-parser.h
+++ gcode-parser.h
@@ -1,4 +1,23 @@
 /*
+ * (c) 2013, 1014 Henner Zeller <h.zeller@acm.org>
+ *
+ * This file is part of BeagleG. http://github.com/hzeller/beagleg
+ *
+ * BeagleG is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BeagleG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BeagleG.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
  * Parser for commong G-codes. Translates movements into absolute mm coordinates
  * and calls callbacks on changes.
  * Simple implementation, by no means complete.
--- shared-constants.h
+++ shared-constants.h
@@ -1,4 +1,22 @@
-// To be read by *.c and *.p file.
+// (c) 2013, 1014 Henner Zeller <h.zeller@acm.org>
+//
+// This file is part of BeagleG. http://github.com/hzeller/beagleg
+//
+// BeagleG is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// BeagleG is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with BeagleG.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+// To be read by *.c and *.p file, so only // comments.
 
 #define STATE_EMPTY 0
 #define STATE_FILLED 1
