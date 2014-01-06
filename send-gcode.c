7786b28b3fb681a3144bd754ed228908dd8d5cc1
Henner Zeller <h.zeller@acm.org>
2014-01-06 04:48:02 +0000

    o not _that_ conservative.

--- send-gcode.c
+++ send-gcode.c
@@ -37,7 +37,7 @@
 #include "gcode-machine-control.h"
 
 // Some default settings.
-#define DEFAULT_MAX_FEEDRATE_MM_PER_SEC 150
+#define DEFAULT_MAX_FEEDRATE_MM_PER_SEC 200
 static const int kStepsPerMM[] = { 160, 160, 160, 40, 0, 0, 0, 0 };
 
 static void print_file_stats(const char *filename,
