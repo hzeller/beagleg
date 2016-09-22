f3613e875baccc0a888d166f827f1d045281c0c2
Henner Zeller <h.zeller@acm.org>
2016-09-21 23:47:34 -0700

    o Move config-file flag into variable, so that we can switch gcode   printing with and w/o machine movement with one comment character.

--- src/test-create-html.sh
+++ src/test-create-html.sh
@@ -13,8 +13,10 @@ THRESHOLD_ANGLE="-t16"
 # The step-speed-different.config _should_ look the same in the output, but
 # currently the output is clearly broken, so let's not use it initially to
 # first fix what is broken alrady.
-BEAGLEG_CONFIG=testdata/step-speed-same.config
-#BEAGLEG_CONFIG=testdata/step-speed-different.config
+# If config is commented out entirely, we only get the gcode output without
+# colored machine move.
+BEAGLEG_CONFIG="-c testdata/step-speed-same.config"
+#BEAGLEG_CONFIG="-c testdata/step-speed-different.config"
 
 GCODE2PS=$(dirname $0)/gcode2ps
 TEST_OUT_DIR=$(dirname $0)/test-out
@@ -25,7 +27,7 @@ rm -f $OUT_HTML
 while [ $# -ne 0 ] ; do
     GCODE_FILE=$1
     BASENAME=$(basename $GCODE_FILE .gcode)
-    $GCODE2PS $THRESHOLD_ANGLE -o $TEST_OUT_DIR/${BASENAME}.ps -c $BEAGLEG_CONFIG -s -T2 $GCODE_FILE
+    $GCODE2PS $THRESHOLD_ANGLE -o $TEST_OUT_DIR/${BASENAME}.ps $BEAGLEG_CONFIG -s -T2 $GCODE_FILE
     if [ $? -ne 0 ] ; then
 	ERRMSG="<span style='color:#ff0000; font-weight:bold;'>Got gcode errors</span>"
     else
