78b261a163fb41b124652550e8d4832c911a75a1
Henner Zeller <h.zeller@acm.org>
2023-03-08 23:31:35 -0800

    Add shell.nix.

---
+++ shell.nix
@@ -0,0 +1,30 @@
+# This is a nix-shell for use with the nix package manager.
+# If you have nix installed, you may simply run `nix-shell`
+# in this repo, and have all dependencies ready in the new shell.
+
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+  buildInputs = with pkgs;
+    [
+      pkg-config
+      git
+      lcov
+      bear
+      gtest
+      valgrind
+
+      yosys
+      surelog
+      yosys-symbiflow.systemverilog
+
+      nextpnr
+      verilator
+
+      clang-tools_11
+    ];
+  shellHook =
+  ''
+    # When compiling on a non-machine, switch off these options.
+    export ARM_COMPILE_FLAGS=""
+  '';
+}
