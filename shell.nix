b9e878812bb93c7dab97ee7df31abcceb9ba2d3f
Henner Zeller <h.zeller@acm.org>
2025-08-24 18:27:43 +0200

    Update clang-tidy config.

--- shell.nix
+++ shell.nix
@@ -1,7 +1,3 @@
-# This is a nix-shell for use with the nix package manager.
-# If you have nix installed, you may simply run `nix-shell`
-# in this repo, and have all dependencies ready in the new shell.
-
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
   buildInputs = with pkgs;
@@ -14,7 +10,7 @@ pkgs.mkShell {
       gtest
       valgrind
       ghostscript
-      clang-tools_15
+      clang-tools_19
       graphviz
     ];
   shellHook =
